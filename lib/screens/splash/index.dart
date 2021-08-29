05a355035816bc61c908b3dcd985540f29058630
kalai <kalaiarasan.ayyanar@gmail.com>
2021-08-29 21:23:09 +0530

    chore : Auto navigation to homescreen from splash screen

--- lib/screens/splash/index.dart
+++ lib/screens/splash/index.dart
@@ -1,8 +1,12 @@
 // packages
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 // widgets
 import 'package:economy_app/components/common_place_holder.dart';
+
+// Constants
+import 'package:economy_app/constants/routes/index.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -12,6 +16,14 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, AppRoutes.HOME_SCREEN);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
